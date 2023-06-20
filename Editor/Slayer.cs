9e70631d60a854744a1b793ece0abb6ecdc75757
Daniel O'Sullivan <dansteamshell@gmail.com>
2023-06-20 06:41:25 +0100

    Update Slayer.cs

--- Editor/Slayer.cs
+++ Editor/Slayer.cs
@@ -7,8 +7,6 @@ using UnityEngine;
                 Application.OpenURL(link);
             }
         }
-
-<<<<<<< Updated upstream
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties) {
         // render the default gui
         base.OnGUI(materialEditor, properties);
@@ -22,16 +20,3 @@ using UnityEngine;
         EditorGUILayout.EndHorizontal();
     }
 }
-=======
-        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties) {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            Slayer.linkButton(70, 20, "Github", "https://github.com/saberslay/SlayerShader");
-            GUILayout.Space(2);
-            Slayer.linkButton(70, 20, "Donate", "https://paypal.me/saberslay");
-            GUILayout.FlexibleSpace();
-            EditorGUILayout.EndHorizontal();
-            base.OnGUI(materialEditor, properties);
-        }
-    }
->>>>>>> Stashed changes
